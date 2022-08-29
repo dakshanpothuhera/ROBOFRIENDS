dd2543464632f439087eaf59b454b1e46ed8aac7
Dakshan Pothuhera <dakshan@datampowered.com.au>
2022-08-29 11:10:30 +1000

    change code to use robots.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(
+
+    <div>
+        <Card id={robots[0].id} name={robots.name[0]} email={robots.email[0]}/>
+        <Card id={robots[1].id} name={robots.name[1]} email={robots.email[1]}/>
+        <Card id={robots[2].id} name={robots.name[2]} email={robots.email[2]}/>
+    </div>
+
+
+, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
